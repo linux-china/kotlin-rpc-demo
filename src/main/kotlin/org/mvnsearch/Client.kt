0664460c9b9f34d0e2ef17cf5d4779074b3996ba
linux_china <libing.chen@gmail.com>
2024-05-23 21:02:33 +0800

    refactor: use installRPC()

--- src/main/kotlin/org/mvnsearch/Client.kt
+++ src/main/kotlin/org/mvnsearch/Client.kt
@@ -1,34 +1,22 @@
 package org.mvnsearch
 
-import io.ktor.client.*
-import io.ktor.client.plugins.websocket.*
-import io.ktor.http.*
+import io.ktor.client.HttpClient
+import io.ktor.client.request.url
 import kotlinx.coroutines.runBlocking
 import kotlinx.rpc.client.withService
 import kotlinx.rpc.serialization.json
+import kotlinx.rpc.transport.ktor.client.installRPC
 import kotlinx.rpc.transport.ktor.client.rpc
 import kotlinx.rpc.transport.ktor.client.rpcConfig
 
 fun main() = runBlocking {
-    val ktorClient = HttpClient {
-        install(WebSockets)
+
+    val rpcClient = HttpClient { installRPC() }.rpc {
+        url("ws://localhost:8080/my-service")
+        rpcConfig { serialization { json() } }
     }
 
-    val client = ktorClient.rpc {
-        url {
-            host = "localhost"
-            port = 8080
-            encodedPath = "my-service"
-        }
-
-        rpcConfig {
-            serialization {
-                json()
-            }
-        }
-    }
-
-    val myService: MyService = client.withService<MyService>()
+    val myService: MyService = rpcClient.withService<MyService>()
     val welcome = myService.hello("Jackie")
     println(welcome)
 }
