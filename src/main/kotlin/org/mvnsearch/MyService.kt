25b2fbabbe688185b7b1c776f186f0c37725a713
linux_china <libing.chen@gmail.com>
2024-05-24 09:27:59 +0800

    chore: add BonusService to show service definition

--- src/main/kotlin/org/mvnsearch/MyService.kt
+++ src/main/kotlin/org/mvnsearch/MyService.kt
@@ -1,6 +1,9 @@
 package org.mvnsearch
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.rpc.RPC
+import kotlinx.serialization.Serializable
 import kotlin.coroutines.CoroutineContext
 
 interface MyService : RPC {
@@ -12,3 +15,16 @@ class MyServiceImpl(override val coroutineContext: CoroutineContext) : MyService
         return "Hello, $name! I'm server!"
     }
 }
+
+interface BonusService : RPC {
+    val liveBonuses: Flow<Int>
+    val total: StateFlow<Int>
+    suspend fun calculateBonuses(order: Order): Int
+    suspend fun sendNotification(event: String)
+    suspend fun sendFlow(flow: Flow<Int>)
+    suspend fun receiveFlow(name: String): Flow<Int>
+    suspend fun channel(flow: Flow<Int>): Flow<Int>
+}
+
+@Serializable
+data class Order(val user: String, val coffeeId: Int)
