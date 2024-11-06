d7e8bb426c66c2bd70e61a395f93a14f98a9ac94
Libing Chen <libing.chen@gmail.com>
2024-11-06 22:10:10 +0800

    build: update to kotlin rpc 0.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.0.10"
-    kotlin("plugin.serialization") version "2.0.10"
-    id("io.ktor.plugin") version "3.0.0-rc-1"
-    id("org.jetbrains.kotlinx.rpc.plugin") version "0.3.0"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
+    id("io.ktor.plugin") version "3.0.1"
+    id("org.jetbrains.kotlinx.rpc.plugin") version "0.4.0"
 }
 
 group = "org.mvnsearch"
@@ -14,13 +14,14 @@ repositories {
 }
 
 application {
-    mainClass.set("ApplicationKt")
+    mainClass.set("org.mvnsearch.ServerKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 dependencies {
+    implementation(platform("org.jetbrains.kotlinx:kotlinx-rpc-bom:0.4.0"))
     implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-client")
     implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-server")
     implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-serialization-json")
@@ -28,11 +29,12 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-client")
     implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-server")
 
-    implementation("io.ktor:ktor-client-cio-jvm")
+    implementation("io.ktor:ktor-client-cio")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("ch.qos.logback:logback-classic:1.5.12")
 
-    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("io.ktor:ktor-server-test-host")
 }
 
 tasks.test {
